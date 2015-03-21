7326d3d095eb2ece5517bcfb4ac95171cad5f721
Yasser H. Daoud <yasserhtd@hotmail.com>
2015-03-21 08:34:57 +0200

    Mod: in Texture::Texture
    Added glGenerateMipmap to generate mipmaps! :)

--- Texturing/Texturing/Texture/texture.cpp
+++ Texturing/Texturing/Texture/texture.cpp
@@ -13,11 +13,15 @@ Texture::Texture(const std::string& fileName, int texUnit_)
 	glActiveTexture(texUnit);
     glGenTextures(1, &m_texture);
     glBindTexture(GL_TEXTURE_2D, m_texture); 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);       
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);       
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	//OpenGL 3.0+ only!
+	glGenerateMipmap(GL_TEXTURE_2D);
+	
 	glBindTexture(GL_TEXTURE_2D, 0); 
 
     stbi_image_free(data);
